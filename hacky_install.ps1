3f6160d12bd180f88d84f1fe91b779e0b433ff6b
Jonathan Chancey <jchancey@ucmerced.edu>
2020-04-16 13:24:34 -0700

    Update install script

--- hacky_install.ps1
+++ hacky_install.ps1
@@ -1,4 +1,6 @@
 $sourceRoot = Join-Path $PSScriptRoot Mod\Valkyrie
 $destinationRoot = "C:\Program Files (x86)\Steam\steamapps\common\Mount & Blade II Bannerlord\Modules\"
+$destinationModFolder = Join-Path $destinationRoot Valkyrie
 
+Remove-Item $destinationModFolder -Recurse
 Copy-Item -Path $sourceRoot -Recurse -Destination $destinationRoot -Container
