86b126468b7b51d46c39cddc6f12ee7ae8eb20f7
Jonathan Chancey <jchancey@ucmerced.edu>
2020-04-16 15:10:24 -0700

    Update install script for name change

--- hacky_install.ps1
+++ hacky_install.ps1
@@ -1,6 +1,6 @@
-$sourceRoot = Join-Path $PSScriptRoot Mod\Valkyrie
+$sourceRoot = Join-Path $PSScriptRoot Mod\Valkyrja
 $destinationRoot = "C:\Program Files (x86)\Steam\steamapps\common\Mount & Blade II Bannerlord\Modules\"
-$destinationModFolder = Join-Path $destinationRoot Valkyrie
+$destinationModFolder = Join-Path $destinationRoot Valkyrja
 
 Remove-Item $destinationModFolder -Recurse
 Copy-Item -Path $sourceRoot -Recurse -Destination $destinationRoot -Container
